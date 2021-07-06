bdc153037d9a57c4f1ba2604b02cf26b7214c481
sysu19351005 <55549955+sysu19351005@users.noreply.github.com>
2021-07-07 00:18:22 +0800

    Add files via upload

---
+++ rfb_esrgan_pytorch/models/discriminator.py
@@ -0,0 +1,69 @@
+#判别器
+import torch
+import torch.nn as nn
+
+
+class DiscriminatorForVGG(nn.Module):
+    def __init__(self, image_size: int = 512) -> None:
+        super(DiscriminatorForVGG, self).__init__()
+
+        feature_map_size = image_size // 32
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),  # input is (3) x 512 x 512
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False),  # state size. (64) x 256 x 256
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1, bias=False),  # state size. (128) x 128 x 128
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1, bias=False),  # state size. (256) x 64 x 64
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1, bias=False),  # state size. (512) x 32 x 32
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1, bias=False),  # state size. (512) x 16 x 16
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * feature_map_size * feature_map_size, 100),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Linear(100, 1)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.features(x)
+        out = torch.flatten(out, 1)
+        out = self.classifier(out)
+
+        return out
+
+
+def discriminator_for_vgg(image_size: int = 512) -> DiscriminatorForVGG:
+    model = DiscriminatorForVGG(image_size)
+    return model
